88ce6549bb6e08bb01f37b252ab236e8f5b5300a
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-04-05 22:01:04 +0600

    header added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//import useCar from '../../hooks/useCar';
-import photo1 from '../../img/2.png'
+import useCar from '../../usehooks/useCar';
+import p1 from '../../img/2.png'
+import HomeReview from '../HomeReview/HomeReview';
  const Home = () => {
-    //const [cars] = useCar();
-    // let getCar= cars.slice(0, 3);
+    const [cars] = useCar();
+    let getCar= cars.slice(0, 3);
 
     return (
         <div>
@@ -15,10 +16,26 @@ import photo1 from '../../img/2.png'
                     <Link to='/books' type="button" className="py-3 px-5 mr-2 mb-2 text-sm font-medium text-white focus:outline-none bg-lime-600 rounded-full border border-gray-200">Latest Collection</Link>
                 </div>
                 <div className='border-2 px-1 py-1 rounded-lg md:order-1 order-2'>
-                    <img className='rounded-lg' src={photo1} alt="" />
+                    <img className='rounded-lg' src={p1} alt="" />
                 </div>
             </div>
-             
+            <div className='bg-[#2e54c5] py-5'>
+                <h1 className='text-4xl text-white'>Customer Reviews</h1>
+                <div className='grid md:grid-cols-3 justify-center items-center w-[70%] mx-auto gap-5 my-10 border-2 px-3 py-3 rounded'>
+                    {
+                        getCar.map(car => <HomeReview
+                            car={car}
+                            key={car.id}
+
+                        ></HomeReview>)
+                    }
+                </div>
+                <div className='mt-5'>
+                    <Link to='/review' type="button" className="py-3 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Sell All Reviews </Link>
+                </div>
+                 <br />
+                
+            </div>
         </div>
     );
 };
--- src/Components/HomeReview/HomeReview.js
+++ src/Components/HomeReview/HomeReview.js
@@ -1,9 +1,9 @@
 import React from 'react';
-const CartReview = ({ car}) => {
+ const HomeReview = ({car}) => {
     const { name, comment, img, ratings } = car;
     return (
         <div >
-            <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 h-80 py-1 px-3">
+            <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 md:h-80 py-1 px-3">
                 <div className="flex flex-col items-center pb-10">
                     <img className="mb-3 w-24 h-24 rounded-full shadow-lg" src={img} alt="" />
                     <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name}</h5>
@@ -14,9 +14,9 @@ const CartReview = ({ car}) => {
 
                 </div>
             </div>
-
+           
         </div>
     );
 };
 
-export default CartReview;
+export default HomeReview;
