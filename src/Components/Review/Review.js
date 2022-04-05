f047991d1397fa15cf5a399615fb6edb14a5b811
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-04-05 22:16:28 +0600

    home cart added

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -3,6 +3,7 @@ import useCar from '../../usehooks/useCar';
 import CartReview from '../CartReview/CartReview';
 
 const Review = () => {
+    //hooks
     const [cars]=useCar();
     return (
         <div className='grid md:grid-cols-3 justify-center items-center md:w-[70%] mx-auto gap-5 md:my-10 px-3 py-3'>
